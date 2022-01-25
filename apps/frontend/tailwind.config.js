8de7a33ecbcd327e90c27c68f67fedcbe67bb942
dat_dm_vn <datdm@ascendsgroup.com>
2022-01-25 19:43:24 +0700

    init project

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    'pages/**/*.{js,ts,jsx,tsx}',
+    '../../packages/ui/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// module.exports = {
+//   content: [
+//     './pages/**/*.{js,ts,jsx,tsx}',
+//     './components/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
